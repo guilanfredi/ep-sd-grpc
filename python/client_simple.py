7ff2d1e6da3ddb85996a0d4b163a427eceebd5b1
Gui-PC\Gui <gui.lanfredi@gmail.com>
2018-06-30 19:44:48 -0300

    implementando métodos básicos

--- python/client_simple.py
+++ python/client_simple.py
@@ -1,5 +1,8 @@
 from __future__ import print_function
 
+import sys
+import math
+
 import grpc
 
 import simple_pb2
@@ -10,12 +13,34 @@ def run():
 	channel = grpc.insecure_channel('localhost:50051')
 	stub = simple_pb2_grpc.DelivererStub(channel)
 
-	num1 = 2
-	num2 = 3
+	numa = 2
+	numb = 3
 
-	response = stub.SumNumbers(simple_pb2.SumRequest(a=num1, b=num2))
-	print("\nSomar dois ints(%d, %d) retornou: %d" % (num1, num2, response.result))
+	response = stub.SumNumbers(simple_pb2.SumRequest(a=numa, b=numb))
+	print("\nSomar dois ints(%d, %d) retornou: %d" % (numa, numb, response.result))
 
+	stub.SumOnServer(simple_pb2.Empty())
+	print("\nSomar dois ints no servidor foi concluído")
+
+	num1 = math.floor(sys.maxsize / 10)
+	num2 = math.floor(sys.maxsize / 10)
+	num3 = math.floor(sys.maxsize / 10)
+	num4 = math.floor(sys.maxsize / 10)
+	num5 = math.floor(sys.maxsize / 10)
+	num6 = math.floor(sys.maxsize / 10)
+	num7 = math.floor(sys.maxsize / 10)
+	num8 = math.floor(sys.maxsize / 10)
+
+	response = stub.SumLong(simple_pb2.SumLongRequest(a=num1, b=num2))
+	print("\nSomar dois longs(%d, %d) retornou: %d" % (num1, num2, response.result))
+
+	response = stub.SumEightLongs(simple_pb2.SumEightLongsRequest(a=num1, b=num2, c=num3, d=num4, e=num5, f=num6, g=num7, h=num8))
+	print("\nSomar oito longs(%d, %d, %d, %d, %d, %d, %d, %d) retornou: %d" % (num1, num2, num3, num4, num5, num6, num7, num8, response.result))
+
+	firstString = "aa"
+
+	response = stub.StringReplace(simple_pb2.StringRequest(a=firstString))
+	print("\n formatar string(%s) retornou: %s" % (firstString, response.result))
 
 if __name__ == '__main__':
 	run()
--- python/server_simple.py
+++ python/server_simple.py
@@ -11,8 +11,26 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 class Deliverer(simple_pb2_grpc.DelivererServicer):
 	
 	def SumNumbers(self, request, context):
-		return simple_pb2.SumResponse(result=request.a * request.b)
-
+		aux = request.a + request.b
+		return simple_pb2.SumResponse(result=aux)
+	
+	def SumOnServer(self, request, context):
+		aux = 2 + 3
+		print(aux)
+		return simple_pb2.Empty()
+	
+	def SumLong(self, request, context):
+		aux = request.a + request.b
+		return simple_pb2.SumLongResponse(result=aux)
+	
+	def SumEightLongs(self, request, context):
+		aux = request.a + request.b + request.c + request.d + request.e + request.f + request.g + request.h 
+		return simple_pb2.SumLongResponse(result=aux)
+	
+	def StringReplace(self, request, context):
+		aux = request.a.upper()
+		return simple_pb2.StringResponse(result=aux)
+	
 def serve():
 	server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 	simple_pb2_grpc.add_DelivererServicer_to_server(Deliverer(), server)
